dfee9404ffd49c036d78125872764b2112137266
Aleko Georgiev <61247544+unamusedchemical@users.noreply.github.com>
2023-12-19 01:03:15 +0200

    [ADD] initial commit

---
+++ BandUpIOS/BandUpIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BandUpIOSApp.swift
+//  BandUpIOS
+//
+//  Created by Aleko Georgiev on 9.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BandUpIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BandUpIOS/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  BandUpIOS
+//
+//  Created by Aleko Georgiev on 9.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("BandUP IOS")
+    }
+}
+
+#Preview {
+    ContentView()
+}
